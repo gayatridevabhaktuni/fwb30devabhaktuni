44e6926394982c6008d3cbe989e082c097427ccf
gayatridevabhaktuni <devabhaktunigayatri@gmail.com>
2021-11-02 23:02:29 -0500

    bonus

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # fwb30devabhaktuni
 This is assignment 10
 Heroku server link : https://fwb30devabhaktuni.herokuapp.com/
+
+class watch
+
+Itemname(String) Quantity(number) price(number)
--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var watchRouter = require('./routes/watch');
+var addmodsRouter = require('./routes/addmods');
+var SelectorRouter = require('./routes/Selector');
 var app = express();
 
 // view engine setup
@@ -21,7 +23,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/watch', watchRouter);
+app.use('/addmods', addmodsRouter);
+app.use('/Selector', SelectorRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
